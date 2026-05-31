627189e6f926a6f1f67eddcaafb2f96b5bb80d93
alvincandrawinata32-code <alvincandrawinata32@gmail.com>
2026-06-01 00:21:31 +0700

    Initial Dart console project

---
+++ bin/ta_strukdat_pbo.dart
@@ -0,0 +1,5 @@
+import 'package:ta_strukdat_pbo/ta_strukdat_pbo.dart' as ta_strukdat_pbo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ta_strukdat_pbo.calculate()}!');
+}
---
+++ test/ta_strukdat_pbo_test.dart
@@ -0,0 +1,8 @@
+import 'package:ta_strukdat_pbo/ta_strukdat_pbo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
