6905be5588f13743e4f7fe6e71a26debbcb254b0
Alvin Candra Winata <alvincandrawinata32@gmail.com>
2026-06-01 19:06:46 +0700

    queue riwayat pencarian

---
+++ lib/structures/queue.dart
@@ -0,0 +1,46 @@
+import '../models/desa.dart';
+
+class QueuePencarian {
+    List<String> _riwayatPencarian = [];
+
+    void tambahPencarian(String pencarian) {
+        _riwayatPencarian.add(pencarian);
+    }
+
+    String? hapusPencarian() {
+        if (_riwayatPencarian.isEmpty) {
+            return null;
+        }
+
+        return _riwayatPencarian.removeAt(0);
+    }
+
+    void tampilRiwayatPencarian() {
+        print('\n===== RIWAYAT PENCARIAN =====');
+
+        if (_riwayatPencarian.isEmpty) {
+            print('Belum ada riwayat pencarian!');
+            return;
+        }
+
+        for (int i = 0; i < _riwayatPencarian.length; i++) {
+            print('${i + 1}. ${_riwayatPencarian[i]}');
+        }
+    }
+
+    void lihatPalingDepan() {
+        if (_riwayatPencarian.isEmpty) {
+            print('Queue kosong!');
+            return;
+        }
+        print('Paling depan: ${_riwayatPencarian[0]}');
+    }
+
+    bool isEmpty() {
+        return _riwayatPencarian.isEmpty;
+    }
+
+    int jumlahData() {
+        return _riwayatPencarian.length;
+    }
+}
