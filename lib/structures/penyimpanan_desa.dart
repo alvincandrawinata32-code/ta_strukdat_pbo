651c4819ce42535319aa5e771428dc35ab05d284
Alvin Candra Winata <alvincandrawinata32@gmail.com>
2026-06-01 15:35:55 +0700

    menambah method tambah data desa

---
+++ lib/structures/penyimpanan_desa.dart
@@ -0,0 +1,41 @@
+import '../models/desa.dart';
+import '../models/pengabdian.dart';
+
+class PenyimpananDesa {
+    final List<Desa> listDesa = [];
+    final Map<int, Desa> mapDesa = {};
+
+    void tambahDesa(Desa desa) {
+        listDesa.add(desa);
+        mapDesa[desa.idDesa] = desa;
+
+        print('\nData desa berhasil ditambahkan!');
+    }
+
+    void tambahRiwayatPengabdian(
+        int idDesa, Pengabdian pengabdian) {
+            if (mapDesa.containsKey(idDesa)) {
+                mapDesa[idDesa]!.tambahPengabdian(pengabdian);
+
+                print('\nRiwayat pengabdian berhasil ditambahkan!');
+            } else {
+                print('\nID desa tidak ditemukan!');
+            }
+        }
+
+    void tampilSemuaDesa() {
+        if (listDesa.isEmpty) {
+            print('\nData desa masih kosong.');
+            return;
+        }
+
+        print('\n===== DATA DESA =====');
+
+        for (int i = 0; i < listDesa.length; i++) {
+            print('\nData ke-${i + 1}');
+            listDesa[i].tampilInfo();
+
+        print('--------------------------');
+        }
+    }
+}
