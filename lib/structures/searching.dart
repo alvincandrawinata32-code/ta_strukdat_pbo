6be3d45ab993384ecd4ad3f0bb8163d7d4a516ed
Alvin Candra Winata <alvincandrawinata32@gmail.com>
2026-06-01 17:56:15 +0700

    menambah linier search

---
+++ lib/structures/searching.dart
@@ -0,0 +1,72 @@
+import '../models/desa.dart';
+
+class Searching {
+    final List<Desa> listDesa;
+
+    Searching(this.listDesa);
+
+    void cariDesaNama(String keyword) {
+        bool ketemu = false;
+
+        print('\n===== HASIL PENCARIAN =====');
+
+        for (int i = 0; i < listDesa.length; i++) {
+            if (listDesa[i]
+            .namaDesa
+            .toLowerCase()
+            .contains(keyword.toLowerCase())) {
+                listDesa[i].tampilInfo();
+
+                print('--------------------------');
+
+                ketemu = true;
+            }
+        }
+
+        if (!ketemu) {
+            print('Data desa tidak ditemukan!');
+        }
+    }
+
+    void cariDesaPotensi(String potensi) {
+        bool ketemu = false;
+
+        print('\n===== HASIL PENCARIAN =====');
+
+        for (int i = 0; i < listDesa.length; i++) {
+
+            if (listDesa[i]
+            .potensi
+            .toLowerCase()
+            .contains(potensi.toLowerCase())) {
+                listDesa[i].tampilInfo();
+
+                print('--------------------------');
+
+                ketemu = true;
+            }
+        }
+
+        if (!ketemu) {
+            print('Data desa tidak ditemukan!');
+        }
+    }
+
+    void cariJumlahPengabdian(int jumlah) {
+        bool ketemu = false;
+
+        print('\n===== HASIL PENCARIAN =====');
+
+        for (int i = 0; i < listDesa.length; i++) {
+            if (listDesa[i].riwayatPengabdian.length == jumlah) {
+                print('Nama Desa : ${listDesa[i].namaDesa}');
+                print('Jumlah Pengabdian : ${listDesa[i].riwayatPengabdian.length}');
+                ketemu = true;
+            }
+        }
+
+        if (!ketemu) {
+            print('Data desa tidak ditemukan!');
+        }
+    }
+}
